edb75e370074186a0739c2d4430997ff093fcda9
zhuL <1161107512@qq.com.com>
2018-04-18 20:09:48 +0800

    搭建

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+/**
+ * Created by zhuliang on 2018/4/18.
+ */
+module.exports = {
+  base: '/wechatframework/',
+  title: '微信架构分析文档',
+  description: '软体构作业',
+  port:8888,
+  themeConfig: {
+    repo: 'ZhuLiangT/wechatframework',
+    sidebar: [
+      ['/', '摘要'],
+      ['/pbStatement/', '问题陈述'],
+      ['/ccContent/', '具体内容'],
+
+    ],
+  }
+}
